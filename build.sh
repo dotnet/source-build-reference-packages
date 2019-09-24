b9d1034762e4343e22ebcec265202e32427ac12b
adaggarwal <aditya.aggarwal@microsoft.com>
2019-09-24 12:12:13 +0000

    wip

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-!/usr/bin/env bash
+!#/usr/bin/env bash
 
 __scriptpath=$(cd "$(dirname "$0")"; pwd -P)
 __log_path="$__scriptpath/artifacts/log"
@@ -13,4 +13,4 @@ __SDK_PATH="$DOTNET_INSTALL_DIR/sdk/$_ReadGlobalVersion"
 export RepoRoot="$__scriptpath/"
 
 $__DOTNET_CMD $__SDK_PATH/MSBuild.dll $__scriptpath/src/targetPacks/buildTargettingPackages.proj /bl:"$__log_path/BuildTargettingPackages.binlog" $@
-$__DOTNET_CMD $__SDK_PATH/MSBuild.dll $__scriptpath/src/referencePackages/buildReferencePackages.proj /bl:"$__log_path/BuildReferencePackages.binlog" $@
+# $__DOTNET_CMD $__SDK_PATH/MSBuild.dll $__scriptpath/src/referencePackages/buildReferencePackages.proj /bl:"$__log_path/BuildReferencePackages.binlog" $@
