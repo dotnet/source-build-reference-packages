faa9450b6dc8b41ff88b13b89cbeeae6f3620233
adaggarwal <aditya.aggarwal@microsoft.com>
2020-01-23 20:50:56 +0000

    cleanup

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ __SDK_PATH="$DOTNET_INSTALL_DIR/sdk/$_ReadGlobalVersion"
 export RepoRoot="$__scriptpath/"
 
 $__DOTNET_CMD $__SDK_PATH/MSBuild.dll $__scriptpath/src/targetPacks/buildTargettingPackages.proj /m /bl:"$__log_path/BuildTargettingPackages.binlog" $@
-# $__DOTNET_CMD $__SDK_PATH/MSBuild.dll $__scriptpath/src/referencePackages/buildReferencePackages.proj /bl:"$__log_path/BuildReferencePackages.binlog" $@
+$__DOTNET_CMD $__SDK_PATH/MSBuild.dll $__scriptpath/src/referencePackages/buildReferencePackages.proj /bl:"$__log_path/BuildReferencePackages.binlog" $@
