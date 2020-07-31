85744c704b971489d2972d80b5ce03949fa5c45d
Davis Goodin <dagood@users.noreply.github.com>
2020-07-31 14:10:42 -0500

    Add intermediate nupkg restore tooling (#133)

---
+++ eng/Microsoft.DotNet.Arcade.Sdk/tasks/ReadSourceBuildIntNupkgDependencies.cs
@@ -0,0 +1,56 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public class ReadSourceBuildIntNupkgDependencies : Task
+    {
+        [Required]
+        public string VersionDetailsXmlFile { get; set; }
+
+        [Output]
+        public ITaskItem[] Dependencies { get; set; }
+
+        public override bool Execute()
+        {
+            var root = XElement.Load(VersionDetailsXmlFile, LoadOptions.PreserveWhitespace);
+            XName qualifiedName(string plainName) => root.GetDefaultNamespace().GetName(plainName);
+
+            var sourceBuildDependencies = root
+                .Elements()
+                .Elements(qualifiedName("Dependency"))
+                .Where(d => d.Element(qualifiedName("SourceBuildRepoName")) != null)
+                .ToArray();
+
+            foreach (var d in sourceBuildDependencies)
+            {
+                if (d.Attribute("Name") == null)
+                {
+                    Log.LogError($"{d} has no Name");
+                }
+                if (d.Attribute("Version") == null)
+                {
+                    Log.LogError($"{d} has no Version");
+                }
+            }
+
+            Dependencies = sourceBuildDependencies
+                .Select(d => new TaskItem(
+                    d.Attribute("Name").Value,
+                    new Dictionary<string, string>()
+                    {
+                        ["Version"] = d.Attribute("Version").Value
+                    }))
+                .ToArray();
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
