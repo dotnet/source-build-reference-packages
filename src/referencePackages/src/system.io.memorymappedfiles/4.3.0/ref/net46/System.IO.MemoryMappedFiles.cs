41a1020889e74f2bb62319b623f02f94d9ababf7
Dan Seefeldt <dseefeld@microsoft.com>
2020-10-21 08:05:18 -0500

    Add a set of prebuilts that are reported as DirectDependencies (#170)
    * Add a set of prebuilts that are reported as DirectDependencies
    
    Microsoft.Win32.Registry,4.7.0
    System.Diagnostics.DiagnosticSource,4.7.0
    System.Data.Common,4.3.0
    System.Net.NameResolution,4.3.0
    System.Reflection.TypeExtensions,4.7.0
    System.Text.Encoding.CodePages,4.7.0
    System.Data.SqlClient,4.8.1
    System.Diagnostics.Contracts,4.3.0
    System.IO.UnmanagedMemoryStream,4.3.0
    System.IO.MemoryMappedFiles,4.3.0
    System.Runtime,4.3.1
    System.Collections.Immutable,1.6.0
    System.Collections.Immutable,1.7.0
    System.Reflection.Metadata,1.7.0
    System.Reflection.Metadata,1.8.0
    
    * Update spacing of generated projects
    
    I updated the generator so as projects are re-generated,
    they will have the new desired format.

---
+++ src/referencePackages/src/system.io.memorymappedfiles/4.3.0/ref/net46/System.IO.MemoryMappedFiles.cs
@@ -0,0 +1,41 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// ------------------------------------------------------------------------------
+// Changes to this file must follow the http://aka.ms/api-review process.
+// ------------------------------------------------------------------------------
+
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Diagnostics;
+using System.IO.MemoryMappedFiles;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: ReferenceAssembly]
+[assembly: AssemblyTitle("System.IO.MemoryMappedFiles")]
+[assembly: AssemblyDescription("System.IO.MemoryMappedFiles")]
+[assembly: AssemblyDefaultAlias("System.IO.MemoryMappedFiles")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft® .NET Framework")]
+[assembly: AssemblyCopyright("© Microsoft Corporation.  All rights reserved.")]
+[assembly: AssemblyFileVersion("4.6.24705.01")]
+[assembly: AssemblyInformationalVersion("4.6.24705.01 built by: SOURCEBUILD")]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyMetadata("", "")]
+[assembly: AssemblyVersion("4.0.1.0")]
+
+[assembly: TypeForwardedTo(typeof(Microsoft.Win32.SafeHandles.SafeMemoryMappedFileHandle))]
+[assembly: TypeForwardedTo(typeof(Microsoft.Win32.SafeHandles.SafeMemoryMappedViewHandle))]
+[assembly: TypeForwardedTo(typeof(System.IO.MemoryMappedFiles.MemoryMappedFile))]
+[assembly: TypeForwardedTo(typeof(System.IO.MemoryMappedFiles.MemoryMappedFileAccess))]
+[assembly: TypeForwardedTo(typeof(System.IO.MemoryMappedFiles.MemoryMappedFileOptions))]
+[assembly: TypeForwardedTo(typeof(System.IO.MemoryMappedFiles.MemoryMappedFileRights))]
+[assembly: TypeForwardedTo(typeof(System.IO.MemoryMappedFiles.MemoryMappedViewAccessor))]
+[assembly: TypeForwardedTo(typeof(System.IO.MemoryMappedFiles.MemoryMappedViewStream))]
+
+
+
