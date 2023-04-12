3ffc0a025a4b639dc047a304a0d117aa1d0962e7
Viktor Hofer <viktor.hofer@microsoft.com>
2023-04-12 20:15:24 +0200

    Update generate.ps1

--- eng/generate.ps1
+++ eng/generate.ps1
@@ -32,7 +32,7 @@ function Get-Help() {
   Write-Host "                                               option above, one per line. If specified, the -package option is ignored."
   Write-Host "  -d|-destination                              A path to the root of the repo to copy source into."
   Write-Host "  -t|-type                                     Type of the package to generate. Accepted values: ref (default) | text."
-  Write-Host "  -x|-excludeDependencies                         Determines if package dependencies should be excluded. Default is false."
+  Write-Host "  -x|-excludeDependencies                      Determines if package dependencies should be excluded. Default is false."
   Write-Host "  -f|-feeds                                    A semicolon-separated list of additional NuGet feeds to use during restore."
   Write-Host "  -h|-help                                     Print help and exit."
 }
