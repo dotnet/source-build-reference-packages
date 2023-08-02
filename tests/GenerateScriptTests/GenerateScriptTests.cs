3fb54d4e0ac8b9975d79e7e1551217b1900aa692
Wenwen <v-wexu@microsoft.com>
2023-08-02 17:27:05 +0800

    Update

--- tests/GenerateScriptTests/GenerateScriptTests.cs
+++ tests/GenerateScriptTests/GenerateScriptTests.cs
@@ -24,6 +24,7 @@ public class GenerateScriptTests
         new object[] { "Microsoft.Extensions.Logging.Abstractions", "7.0.1", PackageType.Reference },
         new object[] { "Microsoft.CodeAnalysis.CSharp", "3.11.0", PackageType.Reference },
         new object[] { "System.Security.Cryptography.Pkcs", "7.0.2", PackageType.Reference },
+        new object[] { "Microsoft.Build.NoTargets", "3.7.0", PackageType.Text },
     };
     
     public string SandboxDirectory { get; set; }
