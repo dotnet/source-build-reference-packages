417c9953c921b03eecb143ae7fb4dec5a5fc4fcc
ellahathaway <ellahath@umich.edu>
2023-11-28 03:42:34 +0800

    Abstract xunit version, check env vars in config

--- tests/SbrpTests/Config.cs
+++ tests/SbrpTests/Config.cs
@@ -12,8 +12,22 @@ internal static class Config
     public const string RepoRootEnv = "SBRP_TESTS_REPO_ROOT";
     public const string BuildTypeEnv = "SBRP_TESTS_BUILD_TYPE";
 
-    public static string RepoRoot { get; } = Environment.GetEnvironmentVariable(RepoRootEnv) ?? 
-        Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.IndexOf("artifacts"));
+    public static string RepoRoot
+    {
+        get
+        {
+            string repoRoot = Environment.GetEnvironmentVariable(RepoRootEnv) ??
+                Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.IndexOf("artifacts"));
+            if (string.IsNullOrWhiteSpace(repoRoot))
+            {
+                throw new InvalidOperationException($"Environment variable {RepoRootEnv} cannot be null, empty, or whitespace.");
+            }
+            return repoRoot;
+        }
+    }
+
+    // public static string RepoRoot { get; } = Environment.GetEnvironmentVariable(RepoRootEnv) ?? 
+    //     Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.IndexOf("artifacts"));
     public static string BuildType { get; } = Environment.GetEnvironmentVariable(BuildTypeEnv) ??
         "Release";
 }
