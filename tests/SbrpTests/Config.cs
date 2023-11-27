bd33306b761cfc32bdb21bd94152940c865d67d2
Ella Hathaway <ellahath@umich.edu>
2023-11-28 05:39:18 +0800

    Clean up Config.cs

--- tests/SbrpTests/Config.cs
+++ tests/SbrpTests/Config.cs
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 using System;
 using System.IO;
@@ -26,8 +25,6 @@ internal static class Config
         }
     }
 
-    // public static string RepoRoot { get; } = Environment.GetEnvironmentVariable(RepoRootEnv) ?? 
-    //     Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.IndexOf("artifacts"));
     public static string BuildType { get; } = Environment.GetEnvironmentVariable(BuildTypeEnv) ??
         "Release";
 }
