ea8cad6679ca519dde6791910f44358b6bd3afc6
Eric StJohn <ericstj@microsoft.com>
2024-01-22 18:14:37 -0800

    Add CollectionBuilderAttribute to System.Collections.Immutable
    CollectionBuilderAttribute is another attribute that's used to light-up
    compiler API.  This attribute is filtered by GenAPI.
    
    Add it back manually to support Collection expressions with immutable
    collections.
    
    https://github.com/dotnet/sdk/issues/33693

---
+++ src/referencePackages/src/system.collections.immutable/8.0.0/System.Collections.Immutable.Manual.cs
@@ -0,0 +1,53 @@
+
+namespace System.Collections.Immutable
+{
+    [Runtime.CompilerServices.CollectionBuilder(typeof(ImmutableList), "Create")]
+    public partial interface IImmutableList<T>
+    {}
+
+    [Runtime.CompilerServices.CollectionBuilder(typeof(ImmutableQueue), "Create")]
+    public partial interface IImmutableQueue<T>
+    {}
+
+    [Runtime.CompilerServices.CollectionBuilder(typeof(ImmutableHashSet), "Create")]
+    public partial interface IImmutableSet<T> 
+    {}
+    
+    [Runtime.CompilerServices.CollectionBuilder(typeof(ImmutableStack), "Create")]
+    public partial interface IImmutableStack<T>
+    {}
+
+    [Runtime.CompilerServices.CollectionBuilder(typeof(ImmutableArray), "Create")]
+    public readonly partial struct ImmutableArray<T> 
+    {}
+    
+    [Runtime.CompilerServices.CollectionBuilder(typeof(ImmutableHashSet), "Create")]
+    public sealed partial class ImmutableHashSet<T>
+    {}
+    
+    [Runtime.CompilerServices.CollectionBuilder(typeof(ImmutableList), "Create")]
+    public sealed partial class ImmutableList<T>
+    {}
+
+    [Runtime.CompilerServices.CollectionBuilder(typeof(ImmutableQueue), "Create")]
+    public sealed partial class ImmutableQueue<T>
+    {}
+
+    [Runtime.CompilerServices.CollectionBuilder(typeof(ImmutableSortedSet), "Create")]
+    public sealed partial class ImmutableSortedSet<T>
+    {}
+
+    [Runtime.CompilerServices.CollectionBuilder(typeof(ImmutableStack), "Create")]
+    public sealed partial class ImmutableStack<T>
+    {}
+}
+
+namespace System.Runtime.CompilerServices
+{
+    internal sealed class CollectionBuilderAttribute(Type builderType, string methodName) : Attribute
+    {
+        public Type BuilderType { get; } = builderType;
+
+        public string MethodName { get; } = methodName;
+    }
+}
