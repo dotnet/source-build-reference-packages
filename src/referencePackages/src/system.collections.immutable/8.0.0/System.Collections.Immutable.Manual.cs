38066edfaad4a6fd9ebe9e5ff0b272af0a2ff1b6
Eric StJohn <ericstj@microsoft.com>
2024-01-23 06:33:00 -0800

    Add header and link to issue

--- src/referencePackages/src/system.collections.immutable/8.0.0/System.Collections.Immutable.Manual.cs
+++ src/referencePackages/src/system.collections.immutable/8.0.0/System.Collections.Immutable.Manual.cs
@@ -1,3 +1,11 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// ------------------------------------------------------------------------------
+// Changes to this file must follow the http://aka.ms/api-review process.
+// ------------------------------------------------------------------------------
+
+// See https://github.com/dotnet/sdk/issues/33693 for why this is necessary.
 
 namespace System.Collections.Immutable
 {
