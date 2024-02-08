099941338e9b6b5065c3967e33314c4f2295bad4
Viktor Hofer <viktor.hofer@microsoft.com>
2024-02-08 12:42:11 +0000

    Manual change because of attribute filtering by GenAPI

--- src/referencePackages/src/system.security.cryptography.xml/7.0.1/lib/netstandard2.0/System.Security.Cryptography.Xml.cs
+++ src/referencePackages/src/system.security.cryptography.xml/7.0.1/lib/netstandard2.0/System.Security.Cryptography.Xml.cs
@@ -300,6 +300,7 @@ namespace System.Security.Cryptography.Xml
 
         public bool IsSynchronized { get { throw null; } }
 
+        [System.Runtime.CompilerServices.IndexerName("ItemOf")] // Adding attribute manually as GenAPI filters it out
         public EncryptionProperty this[int index] { get { throw null; } set { } }
 
         public object SyncRoot { get { throw null; } }
@@ -500,6 +501,7 @@ namespace System.Security.Cryptography.Xml
 
         public bool IsSynchronized { get { throw null; } }
 
+        [System.Runtime.CompilerServices.IndexerName("ItemOf")] // Adding attribute manually as GenAPI filters it out
         public EncryptedReference this[int index] { get { throw null; } set { } }
 
         public object SyncRoot { get { throw null; } }
