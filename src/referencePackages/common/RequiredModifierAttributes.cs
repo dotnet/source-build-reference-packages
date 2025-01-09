d2be119293fd0951f341368652d73792d4b2f9a7
Viktor Hofer <viktor.hofer@microsoft.com>
2025-01-09 22:23:30 +0100

    Fix NuGet.Packaging build

--- src/referencePackages/common/RequiredModifierAttributes.cs
+++ src/referencePackages/common/RequiredModifierAttributes.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#if !NET7_0_OR_GREATER
+
 using System.ComponentModel;
 
 namespace System.Runtime.CompilerServices
@@ -37,3 +39,5 @@ namespace System.Diagnostics.CodeAnalysis
     internal sealed class SetsRequiredMembersAttribute : Attribute
     { }
 }
+
+#endif
