d90294cbba25c66945413aaf35006604c6f40600
Viktor Hofer <viktor.hofer@microsoft.com>
2025-01-09 21:08:15 +0100

    Remove XML comments from attribute classes

--- src/referencePackages/common/RequiredModifierAttributes.cs
+++ src/referencePackages/common/RequiredModifierAttributes.cs
@@ -5,7 +5,6 @@ using System.ComponentModel;
 
 namespace System.Runtime.CompilerServices
 {
-    /// <summary>Specifies that a type has required members or that a member is required.</summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
     [EditorBrowsable(EditorBrowsableState.Never)]
     internal sealed class RequiredMemberAttribute : Attribute
@@ -14,9 +13,6 @@ namespace System.Runtime.CompilerServices
 
 namespace System.Runtime.CompilerServices
 {
-    /// <summary>
-    /// Indicates that compiler support for a particular feature is required for the location where this attribute is applied.
-    /// </summary>
     [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
     internal sealed class CompilerFeatureRequiredAttribute : Attribute
     {
@@ -25,34 +21,18 @@ namespace System.Runtime.CompilerServices
             FeatureName = featureName;
         }
 
-        /// <summary>
-        /// The name of the compiler feature.
-        /// </summary>
         public string FeatureName { get; }
 
-        /// <summary>
-        /// If true, the compiler can choose to allow access to the location where this attribute is applied if it does not understand <see cref="FeatureName"/>.
-        /// </summary>
         public bool IsOptional { get; init; }
 
-        /// <summary>
-        /// The <see cref="FeatureName"/> used for the ref structs C# feature.
-        /// </summary>
         public const string RefStructs = nameof(RefStructs);
 
-        /// <summary>
-        /// The <see cref="FeatureName"/> used for the required members C# feature.
-        /// </summary>
         public const string RequiredMembers = nameof(RequiredMembers);
     }
 }
 
 namespace System.Diagnostics.CodeAnalysis
 {
-    /// <summary>
-    /// Specifies that this constructor sets all required members for the current type, and callers
-    /// do not need to set any required members themselves.
-    /// </summary>
     [AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
     internal sealed class SetsRequiredMembersAttribute : Attribute
     { }
