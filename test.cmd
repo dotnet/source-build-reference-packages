f37a90999eb8b34342f6c5bdd29ea6eb3216a47d
Michael Simons <msimons@microsoft.com>
2025-04-04 20:33:23 +0000

    Update to handle pwsh only installed

--- test.cmd
+++ test.cmd
@@ -1,2 +1,8 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -test -configuration Release %*"
+:: Check if pwsh (PowerShell Core) is available
+where pwsh >nul 2>&1
+if %ERRORLEVEL%==0 (
+    pwsh -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -test -configuration Release %*"
+) else (
+    powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -test -configuration Release %*"
+)
